36fe6c44e5e1c765a6bf5531c5b24985e7a1f9b1
debashish sahu <debashish7862@gmail.com>
2023-03-31 21:10:59 +0530

    yes

---
+++ usercard/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Card from './Components/Card';
+
+function App() {
+  return (
+    <>
+    
+    
+    
+    <Card/>
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ usercard/src/Components/Card.jsx
@@ -0,0 +1,90 @@
+import{useState,useEffect} from 'react'
+import'./Card.css'
+
+
+export default function Card({props}) {
+
+
+
+
+
+    const[product,setProduct]=useState([])
+    const[filtered,setFiltered]=useState([])
+    
+
+
+    useEffect(()=>{
+      userCards();
+
+    },[])
+
+    const userCards=()=>{
+      fetch("https://fakestoreapi.com/products")
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data) 
+        setProduct(data)
+        const finalData=product.slice(0,3)
+        
+        setFiltered(finalData)
+        console.log(filtered)
+        
+       
+
+      })
+    }
+  
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+
+
+    filtered.length>0 &&
+    <div className='mainContainer'>
+      {filtered.map((ele)=>{
+        return(
+          <div className='box'>
+        <img src={ele.image} alt="" />
+        <h4>{ele.title}</h4>
+        <h5>{ele.price}</h5>
+        <button>View Product</button>
+
+      </div>
+      
+
+        )
+      })
+
+
+      }
+
+
+      
+
+      
+
+     
+ 
+    </div>
+    
+  )
+}
+
